db258279df258957c4baa039798ce5845a7cc417
Nagraj <tadingi.nagraj.nagraj7@gmail.com>
2023-05-16 17:40:08 +0530

    first upload

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+export default function App(){
+  const [things, setThings] = React.useState(["Thing 1", "Thing 2"]);
+  
+  function addItem (){
+    const newThingText = `Thing ${things.length +1}`
+    setThings(prevState => [...prevState, newThingText])
+  }
+
+const thingsmap = things.map(thing => <p key={thing}>{thing}</p>)
+
+
+
+  return(
+    <div>
+      <button onClick={addItem}>Add Item</button>
+      {thingsmap}
+    </div>
+  )
+}
